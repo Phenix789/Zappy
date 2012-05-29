edbfedfcd552de9680ef5de88f697bc21064e641
vincent kersuzan <kersuz_v@pc-kersuz-v.(none)>
2012-05-29 04:09:41 +0200

    files created

---
+++ src/session/session_create.c
@@ -0,0 +1,16 @@
+/*
+** session_create.c for zappy in /home/kersuz_v//Zappy/src/session
+** 
+** Made by vincent kersuzan
+** Login   <kersuz_v@epitech.net>
+** 
+** Started on  Tue May 29 04:07:25 2012 vincent kersuzan
+** Last update Tue May 29 04:07:59 2012 vincent kersuzan
+*/
+
+#include	"session.h"
+
+int	session_create(void)
+{
+  return (0);
+}
---
+++ src/session/session_register_in.c
@@ -0,0 +1,19 @@
+/*
+** session_register_in.c for zappy in /home/kersuz_v//Zappy/src/session
+** 
+** Made by vincent kersuzan
+** Login   <kersuz_v@epitech.net>
+** 
+** Started on  Tue May 29 04:03:55 2012 vincent kersuzan
+** Last update Tue May 29 04:05:21 2012 vincent kersuzan
+*/
+
+#include	"session.h"
+
+int	session_register_in(char *instruction, char *mask, t_se_execute_cb cb)
+{
+  (void)instruction;
+  (void)mask;
+  (void)cb;
+  return (0);
+}
