d511b83a7de3f66ec254c5e0282636fb9251be08
quentin duval <duval_q@pc-duval-q.(none)>
2012-05-29 02:25:35 +0200

    mains added and structure done

---
+++ src/client/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for zappy in /home/duval_q//Documents/projects/zappy/src
+** 
+** Made by quentin duval
+** Login   <duval_q@epitech.net>
+** 
+** Started on  Tue May 29 02:22:49 2012 quentin duval
+** Last update Tue May 29 02:24:24 2012 quentin duval
+*/
+
+int	main(int __attribute__((unused))ac, char __attribute__((unused))**av)
+{
+  return (0);
+}
---
+++ src/server/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for zappy in /home/duval_q//Documents/projects/zappy/src
+** 
+** Made by quentin duval
+** Login   <duval_q@epitech.net>
+** 
+** Started on  Tue May 29 02:23:43 2012 quentin duval
+** Last update Tue May 29 02:23:50 2012 quentin duval
+*/
+
+int     main(int __attribute__((unused))ac, char __attribute__((unused))**av)
+{
+  return (0);
+}
