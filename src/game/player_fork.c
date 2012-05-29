48dad79832b551194a85661c94cfc36a73e24f60
Damien Vezant <damien.vezant@epitech.eu>
2012-05-29 08:28:11 +0200

    adding some player side functions

---
+++ src/game/player_fork.c
@@ -0,0 +1,16 @@
+/*
+** player_fork.c for zappy in /home/lukior/Zappy/src/game
+** 
+** Made by damien vezant
+** Login   <vezant_d@epitech.net>
+** 
+** Started on  Tue May 29 08:21:57 2012 damien vezant
+** Last update Tue May 29 08:26:34 2012 damien vezant
+*/
+
+#include	"game.h"
+
+void		player_fork(t_player *player)
+{
+  (void)player;
+}
