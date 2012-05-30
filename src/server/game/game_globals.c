f86573d20b06f8f390b0e6cf6529b840bc66ec50
Damien Vezant <damien.vezant@epitech.eu>
2012-05-30 22:20:23 +0200

    working on game again

---
+++ src/server/game/game_globals.c
@@ -0,0 +1,15 @@
+/*
+** game_globals.h for zappy in /home/lukior/Zappy/includes
+** 
+** Made by damien vezant
+** Login   <vezant_d@epitech.net>
+** 
+** Started on  Wed May 30 22:05:58 2012 damien vezant
+** Last update Wed May 30 22:11:14 2012 damien vezant
+*/
+
+#include	"game.h"
+
+t_map           *team_map;
+t_map           *autoplay_team;
+t_world         game_world;
