eea52350fe7b5cc488634baeb282831de78eaee3
Alexandre Frizac <alexandre.frizac@yahoo.fr>
2012-05-31 16:54:27 +0200

    Please, don't fuck my code ! Revert "Revert "Some modifications about the gui's header & boost::thread add""
    This reverts commit f9ac896dfc3c0d9b25dd48d6de14fcc676d4be2c.

--- src/gui/include/gui.hpp
+++ src/gui/include/gui.hpp
@@ -1,6 +1,31 @@
 #ifndef __GUI_HPP_
 #define __GUI_HPP_
 
-#include "map.hpp"
+#include <boost/thread.hpp>
+#include "data_thread.hpp"
+
+struct thread_gui;
+
+namespace game
+{
+  void		run_gui(data_thread &data);
+  thread_gui	*launch_gui(data_thread &data);
+};
+
+struct thread_gui
+{
+  thread_gui(data_thread &data) : _data(data) { }
+  void	operator()()
+  {
+    game::run_gui(this->_data);
+  }
+public:
+  data_thread	&_data;
+
+private:
+  thread_gui();
+};
+
+
 
 #endif /* __GUI_HPP_ */
