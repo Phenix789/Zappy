3c4f6a5edb43818adb8a8b713f43382fd5e4ca2c
kentrichkof <quentin.duval@epitech.eu>
2012-05-31 03:16:06 +0200

    main done. now time to test

--- src/server/main.c
+++ src/server/main.c
@@ -1,66 +1,20 @@
 /*
-** main.c for zappy in /home/duval_q//Documents/projects/zappy/src
+** main.c for zappy in /home/duval_q//Documents/projects/zappy/src/server
 ** 
 ** Made by quentin duval
 ** Login   <duval_q@epitech.net>
 ** 
-** Started on  Tue May 29 02:23:43 2012 quentin duval
-** Last update Thu May 31 01:23:51 2012 vincent kersuzan
+** Started on  Thu May 31 03:09:00 2012 quentin duval
+** Last update Thu May 31 03:12:46 2012 quentin duval
 */
 
-#include	<stdio.h>
+#include	"kernel.h"
 
-#include	"parser.h"
-#include	"session.h"
-
-void	unit_test(char *data, char *mask)
+int	main()
 {
-  t_command	*command;
-
-  if (!data || !mask)
-    printf("un pointeur d entree au moins est a nul\n");
-  else
-    printf("checking : data = \"%s\"\nwith : mask = \"%s\"\n",
-	 data, mask);
-  if ((command = parser_parse(data, mask)))
-    {
-      int	i = 0;
-
-      while (i < list_size(&command->arguments))
-	{
-	  printf("string %d {value = \"%s\"}\n",
-		 i, parser_get_string(command, i));
-	  printf("int %d {value = \"%d\"}\n",
-		 i, parser_get_int(command, i));
-	  i++;
-	}
-      parser_command_destroy(command);
-    }
-  else
-    printf("dismatch\n");
-}
-
-int     main(void)
-{
-  unsigned int	i;
-
-  session_create();
-  i = 0;
-  while (i < 10)
-    {
-      session_register_in("instruction", "mask", NULL);
-      session_register_out(0, "mask");
-      i++;
-    }
-  session_destroy();
-
-  unit_test("instruction str1 52", "%s %s %d");
-  unit_test("pilou split siooooo", "%ssplit%s");
-  unit_test("pilou 512", "%s %d");
-  unit_test(NULL, "%s%d");
-  unit_test("sdpflspdflpsf", NULL);
-  unit_test("plop sdf sdfsdf", "pilou");
-  unit_test("instruction pilou 13", "%s %s %d");
-  //  unit_test("plop pilou", "%s %");
+  t_kernel	*kernel;
+  kernel = kernel_create();
+  kernel_run();
+  kernel_destroy(kernel);
   return (0);
 }
---
+++ src/test/main.c
@@ -0,0 +1,66 @@
+/*
+** main.c for zappy in /home/duval_q//Documents/projects/zappy/src
+** 
+** Made by quentin duval
+** Login   <duval_q@epitech.net>
+** 
+** Started on  Tue May 29 02:23:43 2012 quentin duval
+** Last update Thu May 31 03:08:19 2012 quentin duval
+*/
+
+#include	<stdio.h>
+
+#include	"parser.h"
+#include	"session.h"
+
+void	unit_test(char *data, char *mask)
+{
+  t_command	*command;
+
+  if (!data || !mask)
+    printf("un pointeur d entree au moins est a nul\n");
+  else
+    printf("checking : data = \"%s\"\nwith : mask = \"%s\"\n",
+	 data, mask);
+  if ((command = parser_parse(data, mask)))
+    {
+      int	i = 0;
+
+      while (i < list_size(&command->arguments))
+	{
+	  printf("string %d {value = \"%s\"}\n",
+		 i, parser_get_string(command, i));
+	  printf("int %d {value = \"%d\"}\n",
+		 i, parser_get_int(command, i));
+	  i++;
+	}
+      parser_command_destroy(command);
+    }
+  else
+    printf("dismatch\n");
+}
+
+int     main(void)
+{
+  unsigned int	i;
+
+  session_create();
+  i = 0;
+  while (i < 10)
+    {
+      session_register_in("instruction", "mask", NULL);
+      session_register_out(0, "mask");
+      i++;
+    }
+  session_destroy();
+
+  unit_test("instruction str1 52", "%s %s %d");
+  unit_test("pilou split siooooo", "%ssplit%s");
+  unit_test("pilou 512", "%s %d");
+  unit_test(NULL, "%s%d");
+  unit_test("sdpflspdflpsf", NULL);
+  unit_test("plop sdf sdfsdf", "pilou");
+  unit_test("instruction pilou 13", "%s %s %d");
+  //  unit_test("plop pilou", "%s %");
+  return (0);
+}
