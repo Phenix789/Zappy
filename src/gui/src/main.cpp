19c6fea6855db8b275bb12c288627a813c8d0e17
Alexandre Frizac <alexandre.frizac@yahoo.fr>
2012-05-31 18:14:16 +0200

    Network work. Can read & write on a socket.

--- src/gui/src/main.cpp
+++ src/gui/src/main.cpp
@@ -5,15 +5,50 @@
 ** Login   <duval_q@epitech.net>
 **
 ** Started on  Tue May 29 02:23:59 2012 quentin duval
-// Last update Wed May 30 20:51:32 2012 Alexandre Frizac
+// Last update Thu May 31 18:04:11 2012 Alexandre Frizac
 */
 
 #include "gui.hpp"
+#include "network.hpp"
+
+int	main_gui(const std::string host, const std::string port)
+{
+  data_thread	data;
+  network	sock(host, port);
+
+  if (!sock)
+    {
+      std::cout << "Erreur Network : Impossible de se connecter" << std::endl;
+      return (EXIT_FAILURE);
+    }
+  /*
+  if (init_data(data) == false)
+    return (EXIT_FAILURE);
+  game::launch_gui(data);
+  return (boucle_network(data));
+  */
+  std::string	buffer;
+  //  while (sock)
+  {
+    buffer.clear();
+    getline(sock, buffer);
+    sock << "GRAPHIC\n" << std::flush;
+    std::cout << buffer << std::endl;
+  }
+  sock.close();
+  return (EXIT_SUCCESS);
+}
+
 
 int     main(int __attribute__((unused))ac, char __attribute__((unused))**av)
 {
-  data_thread	data;
-
-  game::launch_gui(data);
-  return (EXIT_SUCCESS);
+  if (ac == 3)
+    return (main_gui(av[1], av[2]));
+  std::cout << "Usage : " << av[0] << " [host] [port]" << std::endl;
+  return (EXIT_FAILURE);
 }
+
+/*
+data_thread data;
+game::launch_gui(data);
+*/
