c0f764b5792d37f42d35a7cb448a4acfaafa639b
Claude Ramseyer <claude.ramseyer@epitech.eu>
2012-06-01 12:24:31 +0200

    call world dump

--- src/server/game/game_world_dump.c
+++ src/server/game/game_world_dump.c
@@ -1,9 +1,9 @@
 /*
 ** game_world_dump.c for zappy in /home/lukior/Zappy/src/server/game
-** 
+**
 ** Made by damien vezant
 ** Login   <vezant_d@epitech.net>
-** 
+**
 ** Started on  Fri Jun  1 12:00:01 2012 damien vezant
 ** Last update Fri Jun  1 12:00:02 2012 damien vezant
 */
@@ -19,19 +19,20 @@ void		game_world_dump()
 
   idx = 0;
   idy = 0;
+  printf("World dump\n");
   while (idy < g_game_world->dimensions.y)
   {
     while (idx < g_game_world->dimensions.x)
     {
       tile = &g_game_world->world[idx * idy];
       assets = &g_game_world->item_count;
-      printf("tile [%d:%d]\nnb players %d\nlinemate %d\nderaumere %d\nsibur %d\n",
-	     tile->coord.x, tile->coord.y, list_size(&tile->players), assets->linemate, assets->deraumere, assets->sibur);
-      printf("mendiane %d\nphiras %d\nthystame %d\nfood %d\n\n",
+      printf("{%i %i:%i:%i:%i:%i:%i:%i} ", list_size(&tile->players),
+	     assets->linemate, assets->deraumere, assets->sibur,
 	     assets->mendiane, assets->phiras, assets->thystame, assets->food);
       ++idx;
     }
-    x = 0;
+    printf("\n");
+    idx = 0;
     ++idy;
   }
 }
