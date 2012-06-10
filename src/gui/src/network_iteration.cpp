bc7c2f1ed92814c6d11fcf3e716de1c1653c85ed
Alexandre Frizac <alexandre.frizac@epitech.eu>
2012-06-10 14:45:45 +0200

    network::iteration throw exception now & position/position.cpp fixed

--- src/gui/src/network_iteration.cpp
+++ src/gui/src/network_iteration.cpp
@@ -4,18 +4,17 @@
 
 namespace network
 {
-  bool	iteration(game::data &data, INetwork &sock, parser &pars)
+  void	iteration(game::data &data, INetwork &sock, parser &pars)
   {
     std::string	buffer;
 
     if (!sock == false)
-      return (false);
+      throw network::except("Connection lost");
     if (sock.isReady(INetwork::WRITE))
       sock.send(data.ask);
     if (sock.isReady(INetwork::READ))
       sock.receive(buffer);
     pars.parse(buffer);
     pars.interpret(data);
-    return (true);
   }
 };
